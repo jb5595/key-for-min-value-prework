290df93c029b44e8b4ca49ebb2886a47e4fc84bb
Jacob Berman <jacobberman1995@gmail.com>
2018-08-12 20:18:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,22 @@
 def key_for_min_value(name_hash)
   if name_hash == {}
     return nil 
-  end 
+
 else 
+  min = ""
   counter = 0
   name_hash.collect do |key, value|
     if counter == 0 
-      min = value
+      min_value = value
+      min = key
     
   else 
-    if
+    if value < min
+      min_value = value
+      min = key
+      
+  end
+end
   
 
 end
